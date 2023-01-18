8f503ab74818f8df61191087b9e1947ed3791185
Ikbal Demirdoven <ikbaldemirdoven@icloud.com>
2023-01-18 00:24:48 -0800

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Ikbal Demirdoven on 2023-01-18.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
